046721ca5729966af0c57b9cc5a77256b511af82
Stelvinsaji <87966154+Stelvinsaji@users.noreply.github.com>
2021-09-25 18:50:41 +0530

    Add files via upload

---
+++ build/precache-manifest.4c51435d44eafe723725d5e2521b4b90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6455904289225220509996ca8af56a66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93cd77c0e03f7fdad6d1",
+    "url": "/static/css/main.71741438.chunk.css"
+  },
+  {
+    "revision": "79811a77af7d9c31a54f",
+    "url": "/static/js/2.623516d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.623516d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93cd77c0e03f7fdad6d1",
+    "url": "/static/js/main.abca7d4a.chunk.js"
+  },
+  {
+    "revision": "3ee6d9289089d700c3ad",
+    "url": "/static/js/runtime-main.2d4e65bc.js"
+  }
+]);
